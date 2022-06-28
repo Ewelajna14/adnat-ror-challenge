84108f69d4041227b6de028edff34bb66ef3ab7e
Ewelina <ekowalczyk1994@gmail.com>
2022-06-28 10:21:02 -0500

    user controller was created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
     belongs_to :organization
     has_many :shifts
 
+    validates :name, presence: true
+    validates :email_address, presence: true
+  
     has_secure_password
+    
 end
