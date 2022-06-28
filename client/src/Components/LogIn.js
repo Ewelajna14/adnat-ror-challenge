8b5da0207c6d88f7f89951d66f197ee7b50b47d1
Ewelina <ekowalczyk1994@gmail.com>
2022-06-28 08:48:04 -0500

    component LogIn was created

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,6 +1,21 @@
+import {Link} from 'react-router-dom'
+
 function LogIn(){
     return(
         <div>
+            <form>
+                <h1>Adnat</h1>
+                <h2>Log in</h2>
+                <label for="email">Email</label><br/>
+                <input type="text" name="email"></input><br/>
+                <label for="password" >Password</label><br/>
+                <input type="password" name="password"></input><br/>
+                <input type="checkbox" id="rememberMe"></input><br/>
+                <label for ="rememberMe"> Remember me</label><br/>
+                <input type="submit" value="Log in"></input><br/>
+                <a href="/">Sign up</a> <br/>
+                <a href="/">Forgot your password?</a>
+            </form> 
 
         </div>
     )
