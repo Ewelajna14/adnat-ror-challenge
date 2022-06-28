9dc420e42d9d8870b8e836f1aa846a3f7687de34
Ewelina <ekowalczyk1994@gmail.com>
2022-06-28 10:32:48 -0500

    session controller was created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    #post /users
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    private
+    def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+    end
 end
