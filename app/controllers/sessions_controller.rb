c973e634df6b31d23bf90938c653aa22dd830ec0
Ewelina <ekowalczyk1994@gmail.com>
2022-06-28 16:10:46 -0500

    implement log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     skip_before_action :authorize, only: :create
     #POST /login
     def create
-        user = User.find_by(email_address, params[:email_address])
+        user = User.find_by(email_address: params[:email_address])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             render json: user, status: :created
--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,16 +1,46 @@
 import {Link} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 
-function LogIn(){
+function LogIn({onLogin}){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e)=>{
+     e.preventDefault()
+     const user={
+        email_address: email,
+        password: password
+     }
+     console.log(user)
+
+    fetch("/login", {
+        method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+     })
+     .then((r)=>{
+        if(r.ok){
+            r.json().then((user)=>onLogin(user))
+        }
+        else{
+            r.json().then((error)=> console.log(error))
+        }
+     })
+
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Adnat</h1>
                 <h2>Log in</h2>
                 <label for="email">Email</label><br/>
-                <input type="email" name="email"></input><br/>
+                <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input><br/>
                 <label for="password" >Password</label><br/>
-                <input type="password" name="password"></input><br/>
+                <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input><br/>
                 <input type="checkbox" id="rememberMe"></input><br/>
                 <label for ="rememberMe"> Remember me</label><br/>
                 <input type="submit" value="Log in"></input><br/>
