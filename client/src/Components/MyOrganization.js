5dfa25edc7f79b7e5d21587e39b0492ab5fbd974
Ewelina <ekowalczyk1994@gmail.com>
2022-06-29 12:59:44 -0500

    myOrganization component was created

---
+++ client/src/Components/MyOrganization.js
@@ -0,0 +1,10 @@
+function MyOrganization({user}){
+    return(
+        <div>
+            <h1>Adnat</h1>
+            <p>Logged in as {user.name}</p>
+        </div>
+    )
+}
+
+export default MyOrganization
