65b4738b379ea3ea063aeccd1f4492088bb852c1
Ewelina <ekowalczyk1994@gmail.com>
2022-07-01 17:39:22 -0500

    shifts were sortest with newest on top

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,9 +1,10 @@
 class Shift < ApplicationRecord
     belongs_to :user
     
-
     validates :break_length, numericality: { only_integer: true }
     validates :start, presence: true
     validates :finish, presence: true
+
+    default_scope {order(start: :desc)}
    
 end
