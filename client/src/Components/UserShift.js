594e9038c9fcf9d7b2713758605f032a63ecc277
Ewelina <ekowalczyk1994@gmail.com>
2022-07-01 13:27:05 -0500

    calculate cost

--- client/src/Components/UserShift.js
+++ client/src/Components/UserShift.js
@@ -20,12 +20,12 @@ function UserShift({shift, user}){
    const diff = (start, end)=> {
     start = start.split(":");
     end = end.split(":");
-    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
-    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
-    var diff = endDate.getTime() - startDate.getTime() - breakMiliSEc;
-    var hours = Math.floor(diff / 1000 / 60 / 60);
+    let startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    let endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    let diff = endDate.getTime() - startDate.getTime() - breakMiliSEc;
+    let hours = Math.floor(diff / 1000 / 60 / 60);
     diff -= hours * 1000 * 60 * 60;
-    var minutes = Math.floor(diff / 1000 / 60);
+    let minutes = Math.floor(diff / 1000 / 60);
 
     if (hours < 0)
        hours = hours + 24;
@@ -33,11 +33,24 @@ function UserShift({shift, user}){
     return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
 }
 
-    const difference = diff(sTime, fTime)
-
     const rate = user.organization.hourly_rate
-    console.log(rate)
-    console.log(difference)
+   
+    const calculateCost = (start, end)=> {
+        start = start.split(":");
+        end = end.split(":");
+        let startDate = new Date(0, 0, 0, start[0], start[1], 0);
+        let endDate = new Date(0, 0, 0, end[0], end[1], 0);
+        let diff = endDate.getTime() - startDate.getTime() - breakMiliSEc;
+        let hours = Math.floor(diff / 1000 / 60 );
+        let minRate = rate / 60
+        let cost = hours * minRate
+        return Math.round(cost * 100) / 100
+    } 
+
+     const cost = calculateCost(sTime, fTime)
+
+
+    const difference = diff(sTime, fTime)
 
     return(
         <tr>
@@ -47,6 +60,7 @@ function UserShift({shift, user}){
           <td>{finish}</td>
           <td>{shift.break_length}</td>
           <td>{difference}</td>
+          <td>{cost}</td>
         </tr>
     )
 }
